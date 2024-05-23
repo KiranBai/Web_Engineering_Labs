db634b3f42432a9da0cb9f516657c2bb1fb0f10e
KiranBai <157038014+KiranBai@users.noreply.github.com>
2024-05-23 23:34:11 +0500

    Add files via upload

---
+++ Web_Lab_14/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import Qamyab from './Qamyab.jsx'
+import TodoList from './TodoList.jsx'
+import Users from './Users.jsx'
+import Mathshapes from './Mathshapes.jsx'
+import Friends from './Friends.jsx'
+import State from './State.jsx'
+import Header from './Header.jsx'
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Header />
+  </React.StrictMode>,
+)
